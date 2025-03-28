cb7680564e748f2f3e4f6f5db69c156ae6694814
divanshu989 <133862873+divanshu989@users.noreply.github.com>
2025-03-29 00:50:45 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
